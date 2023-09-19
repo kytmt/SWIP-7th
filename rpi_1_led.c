383bebed584a9fbd1f44b648f19d331f2caabb41
[yws] <[yatt0716@gmail.com]>
2023-09-19 13:47:33 +0900

    led.c_0919_3st

--- rpi_1_led.c
+++ rpi_1_led.c
@@ -6,23 +6,22 @@ void blinkLED(int mode)
 
     pinMode(GPIO_LED, OUTPUT);
 
-    while(1) {
-        if (mode == BLINK_1s)
-        {
-            digitalWrite(GPIO_LED, HIGH);
-            delay(1000);
-            
-            digitalWrite(GPIO_LED, LOW);
-            delay(1000);
-        }
 
-        else if (mode = BLINK_10Hz)
-        {
-            digitalWrite(GPIO_LED, HIGH);
-            delay(100);
-            
-            digitalWrite(GPIO_LED, LOW);
-            delay(100);
-        }
+    if (mode == BLINK_1s)
+    {
+        digitalWrite(GPIO_LED, HIGH);
+        delay(1000);
+        
+        digitalWrite(GPIO_LED, LOW);
+        delay(1000);
+    }
+
+    else if (mode = BLINK_10Hz)
+    {
+        digitalWrite(GPIO_LED, HIGH);
+        delay(100);
+        
+        digitalWrite(GPIO_LED, LOW);
+        delay(100);
     }
 }
