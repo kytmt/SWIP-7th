ec1d4b2bf3837f7593f2f981a93cb5373458f9c0
[yws] <[yatt0716@gmail.com]>
2023-09-19 13:06:48 +0900

    led.h_0919_2st

--- rpi_1_led.h
+++ rpi_1_led.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
+#define GPIO_LED 4 // LED pin Number
 #define BLINK_1s 0 // blinking every 1 second
 #define BLINK_10Hz 1 // blinking 10Hz when detected within 20cm
 
