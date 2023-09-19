60a6095486274e741d04973d2f66a150739a0f00
bjy <bjt@naver.com>
2023-09-19 10:37:03 +0900

    `

--- rpi_2_motor.c
+++ rpi_2_motor.c
@@ -5,39 +5,27 @@
 #include "rpi_2_motor.h"
 
 
-void moveMotor(int degree)
+int moveMotor(int inputValue)
 {
     float dutyCycle;        // the unit is %
     int pulseWidth;
+
+    if(inputValue < 0){
+        printf("The inputValue is smaller than 0")
+        return -1;
+    }
+    wiringPiSetupGpio();
+    pinMode(PIN, OUTPUT);
+
+    // Set PWM range (= the total number of clock ticks during a period)
+    softPwmCreate(PIN, 0, PWM_RANGE);
 
     dutyCycle = ((float)(degree*5)/180 + 5.0);
     pulseWidth = dutyCycle*(PWM_RANGE/100);
 
     softPwmWrite(PIN, pulseWidth);
 
-    printf("RPC request 'moveMotor(120)' received and processed.");
-    printf("\n");
-
-    return;
-}
-
-int R2_motor(void) 
-{
-    int inputValue;         // the unit is degree
-    
-    wiringPiSetupGpio();
-    pinMode(PIN, OUTPUT);
-
-    // Set PWM range (= the total number of clock ticks during a period)
-    softPwmCreate(PIN, 0, PWM_RANGE);
-  
-    inputValue = 180;
-    moveMotor(inputValue);
-    delay(600);
-    inputValue = 0;
-    moveMotor(inputValue);
     delay(600);
 
-
-    return 1;
+    return 0;
 }
--- rpi_2_motor.h
+++ rpi_2_motor.h
@@ -1,5 +1,7 @@
 void moveMotor(int degree);
 int R2_motor(void) ;
 
+int inputValue;
+
 #define PIN 18
 #define PWM_RANGE 200
